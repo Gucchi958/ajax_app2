89db04d021aa814715f33a5d316060972142d417
Gucchi958 <81725643+Gucchi958@users.noreply.github.com>
2021-04-16 11:55:34 +0900

    コントローラーにリクエストを送信する

---
+++ app/javascript/memo.js
@@ -0,0 +1,17 @@
+const { href } = require("@rails/ujs");
+
+function post(){
+  const submit = document.getElementById("submit");
+  submit.addEventListener("click", (e) =>{
+    e.preventDefault();
+    const form = document.getElementById("form");
+    const formData = new FormData(form);
+    const XHR = new XMLHttpRequest();
+    XHR.open("POST", "/posts", true);
+    XHR.responseTypw = "json";
+    XHR.send(formData);
+
+  });
+}
+
+window.addEventListener('load', post);
